3ab24dd31badb658756affed29b6333e53f42574
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-01-17 11:59:35 -0300

    resolvendo exercicio

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 
-import Layout from "@/components/layout"; // Importando o Layout da pasta src - components - layout.jsx
+import Layout from "@/components/ui/Layout"; // Importando o Layout da pasta src - components - layout.jsx
 
 export default function App({ Component, pageProps }) {
   return (

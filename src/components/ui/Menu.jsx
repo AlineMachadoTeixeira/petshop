95ead1646af0cc15ac82620844b2c2a327553240
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-01-18 11:32:42 -0300

    Update Menu.jsx

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -53,13 +53,13 @@ const StyledNav = styled.nav`
       background-color: var(--cor-primaria-fundo-hover);
     }
 
-    a.ativo {
-      background-color: red;
-    }
-
     @media screen and (min-width: 700px) {
       padding-left: 2rem;
       padding-right: 2rem;
     }
+  }
+
+  a.ativo {
+    background-color: red;
   }
 `;

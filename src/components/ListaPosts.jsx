e88a6aada51d0abfb2bfdaf4d40719395fad46f0
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-01-22 08:43:46 -0300

    refazendo o desafio 10

--- src/components/ListaPosts.jsx
+++ src/components/ListaPosts.jsx
@@ -1,12 +1,57 @@
 import Link from "next/link";
+import styled from "styled-components";
 
-export default function ListaPosts({ post }) {
+/*  ListaPosts({ posts }) esse posts é um nome que escolhemos podemos colocar array / lista e etc  */
+export default function ListaPosts({ posts }) {
   return (
-    <article key={post.id}>
-      <Link href="">
-        <h3> {post.titulo} </h3>
-        <p> {post.subtitulo} </p>
-      </Link>
-    </article>
+    <StyledListaPosts>
+      {/* Antes o posts.map era arrayPosts.map  e estava na index e passamos para cá por causa do desafio*/}
+      {posts.map((post) => {
+        return (
+          <article key={post.id}>
+            <Link href="">
+              <h3> {post.titulo} </h3>
+              <p> {post.subtitulo} </p>
+            </Link>
+          </article>
+        );
+      })}
+    </StyledListaPosts>
   );
 }
+
+const StyledListaPosts = styled.div`
+  article {
+    background-color: #f7f7f7;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    box-shadow: var(--sombra-box);
+    border-radius: var(--borda-arredondada);
+    transition: transform 200ms;
+
+    & a {
+      text-decoration: none;
+      color: black;
+
+      &:hover,
+      &:focus {
+        color: var(--cor-logo);
+      }
+    }
+  }
+
+  article:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+  }
+
+  @media screen and (min-width: 500px) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    article {
+      width: 49%;
+    }
+  }
+`;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,51 +21,12 @@ export default function Home() {
       {/* //Antes era <section> mudamos por causa do css */}
       <StyledHome>
         <h2>Pet Notícias</h2>
-        <StyledListaPosts>
-          {arrayPosts.map((post) => {
-            return <ListaPosts post={post} key={post.id} />;
-          })}
-        </StyledListaPosts>
+        {/* arrayPosts vem da pasta api / array-posts */}
+        <ListaPosts posts={arrayPosts} />
       </StyledHome>
     </>
   );
 }
-
-const StyledListaPosts = styled.div`
-  article {
-    background-color: #f7f7f7;
-    padding: 1rem;
-    margin-bottom: 1rem;
-    box-shadow: var(--sombra-box);
-    border-radius: var(--borda-arredondada);
-    transition: transform 200ms;
-
-    & a {
-      text-decoration: none;
-      color: black;
-
-      &:hover,
-      &:focus {
-        color: var(--cor-logo);
-      }
-    }
-  }
-
-  article:hover {
-    cursor: pointer;
-    transform: scale(1.05);
-  }
-
-  @media screen and (min-width: 500px) {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-
-    article {
-      width: 49%;
-    }
-  }
-`;
 
 const StyledHome = styled.section`
   h2::before {

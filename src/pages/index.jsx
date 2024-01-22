b816fb5250001b4decdc4510cd8d1754373821c6
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-01-22 09:40:27 -0300

    deletamos o import arrayPosts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styled from "styled-components";
-import arrayPosts from "./api/array-posts";
+
 import ListaPosts from "@/components/ListaPosts";
 
 export default function Home() {

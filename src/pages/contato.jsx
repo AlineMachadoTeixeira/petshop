a675920486bfa81a1b6025759729611813b8d76f
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-01-31 09:28:07 -0300

    Ajuste para validação

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -59,38 +59,48 @@ export default function Contato() {
             <div>
               <label htmlFor="nome">Nome:</label>
               <input
-                {...register("nome")}
+                {...register("nome", { required: true })}
                 type="text"
                 name="nome"
                 id="nome"
-                required
               />
             </div>
+
+            {/* ? é conhecido como "Optional Chaining [encadeamento opcional]" É usado para evitar erros caso uma propriedade de um objeto seja null ou undefined. Caso não seja null/undefined, ai sim verificamos se o type é required para seguir com a validação.  */}
+            {errors.nome?.type == "required" && <p>Você deve digitar o nome</p>}
+
             <div>
               <label htmlFor="email">E-mail:</label>
               <input
-                {...register("email")}
+                {...register("email", { required: true })}
                 type="email"
                 name="email"
                 id="email"
-                required
               />
             </div>
+            {errors.email?.type == "required" && (
+              <p>Você deve digitar o e-mail</p>
+            )}
 
             <div>
               <label htmlFor="menssagem">
                 Menssagem: <br />
               </label>
               <textarea
-                {...register("mensagem")}
+                {...register("mensagem", { required: true, minLength: 20 })}
                 name="mensagem"
                 id="mensagem"
                 maxLength={500}
                 cols="30"
                 rows="10"
-                required
               ></textarea>
             </div>
+            {errors.mensagem?.type == "required" && (
+              <p>Você deve digitar uma mensagem</p>
+            )}
+            {errors.mensagem?.type == "minLength" && (
+              <p>Escreva pelo menos 20 caracteres</p>
+            )}
             <div>
               <button type="submit">Enviar menssagem</button>
             </div>
@@ -112,6 +122,13 @@ const StyledContato = styled.section`
     margin-bottom: 0.5rem;
     display: flex;
     justify-content: space-between;
+
+    /* Seletor + significa "elemento adjacente ou seja, pegar os parágrafos que estão depois da div" */
+    & + p {
+      color: red;
+      font-size: 0.8rem;
+      font-style: italic;
+    }
 
     & label {
       font-weight: bold;
